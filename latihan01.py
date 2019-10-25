e87b5a24becab2919974713a91c6224f9f9bbb93
riwanmanurung <56192368+riwanmanurung@users.noreply.github.com>
2019-10-25 18:21:55 +0700

    Add files via upload

---
+++ latihan01.py
@@ -0,0 +1,11 @@
+#ini Variabel & Nilai
+
+namalengkap = "riwan irosucipto manurung"
+namapanggilan = "riwan"
+npm = "311910500"
+tempatlahir = "aekbontar"
+umur = "19"
+telf = "0821 6011 2313"
+alamat = "cikarang"
+
+print("assalamualikum, \n let me introduce my self, My name is",namalengkap,",but you can call me",namapanggilan,". My NPM is",npm,". i was born in",tempatlahir,"and i am",umur,"years old. I am very glad if you want to invite my house in",alamat,". So, don't forget to call me before with the number",telf,"\n Thank you")
